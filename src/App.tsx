f99584efda81babc7667a63563ed6ff4bcc20b2a
Tejas Kedare <tejaskedare.22@gmail.com>
2025-11-23 00:30:27 +0530

    vercel changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <p>Start prompting (or editing) to see magic happen :)</p>
-    </div>
+    <>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <p>Start prompting (or editing) to see magic happen :)</p>
+      </div>
+      <SpeedInsights />
+    </>
   );
 }
 
